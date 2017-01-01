eed9136ce3be9f3b377d287f02d03595f150a71a
Brooke Kuhlmann <brooke@alchemists.io>
2017-01-01 15:06:46 -0700

    Updated to NPM Setup 2.1.0.

--- lib/settings.sh
+++ lib/settings.sh
@@ -16,7 +16,7 @@ export MRI=2.4.0
 # Repositories
 export REPO_RUBY_SETUP=v7.0.0
 export REPO_GO_SETUP=v2.1.0
-export REPO_NPM_SETUP=v2.0.0
+export REPO_NPM_SETUP=v2.1.0
 export REPO_SUBLIME_TEXT_SETUP=v6.0.0
 export REPO_DOTFILES=v24.2.0
 
