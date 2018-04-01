831c00bb50ed4f9225a85ecc163dd45114fe32aa
Brooke Kuhlmann <brooke@alchemists.io>
2018-04-01 11:01:17 -0600

    Updated to Dotfiles 31.2.0.
    [Changes](https://is.gd/cvMScS).

--- lib/settings.sh
+++ lib/settings.sh
@@ -17,7 +17,7 @@ export MRI=2.5.1
 export REPO_RUBY_SETUP=10.1.0
 export REPO_YARN_SETUP=1.1.0
 export REPO_SUBLIME_TEXT_SETUP=8.1.0
-export REPO_DOTFILES=v31.0.0
+export REPO_DOTFILES=31.2.0
 
 # Applications
 export AUDIO_BRIDGE_APP_NAME="AudioBridge.app"
