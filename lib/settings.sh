260a67a199e321b447e695629a2c0217c81a884f
Brooke Kuhlmann <brooke@alchemists.io>
2019-01-01 12:27:59 -0700

    Updated to Dotfiles 32.3.0.
    [Changes](https://github.com/bkuhlmann/dotfiles/releases/tag/32.3.0).

--- lib/settings.sh
+++ lib/settings.sh
@@ -15,7 +15,7 @@ export MRI=2.6.0
 export REPO_RUBY_SETUP=12.0.0
 export REPO_YARN_SETUP=1.3.0
 export REPO_SUBLIME_TEXT_SETUP=9.0.0
-export REPO_DOTFILES=32.2.0
+export REPO_DOTFILES=32.3.0
 
 # Applications
 export AUDIO_BRIDGE_APP_NAME="AudioBridge.app"
