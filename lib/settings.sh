1c93d1bb622ebbb3e06a19ecadb4b6fb87bde81b
Brooke Kuhlmann <brooke@alchemists.io>
2019-02-01 19:33:25 -0700

    Updated to Sublime Text Setup 9.1.0.
    [Changes](https://is.gd/Sw1u9Y).

--- lib/settings.sh
+++ lib/settings.sh
@@ -14,7 +14,7 @@ export MRI=2.6.1
 # Repositories
 export REPO_RUBY_SETUP=12.0.0
 export REPO_YARN_SETUP=1.3.0
-export REPO_SUBLIME_TEXT_SETUP=9.0.0
+export REPO_SUBLIME_TEXT_SETUP=9.1.0
 export REPO_DOTFILES=32.3.0
 
 # Applications
