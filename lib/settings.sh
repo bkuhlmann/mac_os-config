2928556cf02046eb6591992422f79beadc792b27
Brooke Kuhlmann <brooke@alchemists.io>
2019-03-01 06:41:31 -0700

    Updated to Ruby Setup 12.1.0.
    [Details](https://github.com/bkuhlmann/ruby_setup/releases/tag/12.1.0).

--- lib/settings.sh
+++ lib/settings.sh
@@ -12,7 +12,7 @@ IFS=$'\n\t'
 export MRI=2.6.1
 
 # Repositories
-export REPO_RUBY_SETUP=12.0.0
+export REPO_RUBY_SETUP=12.1.0
 export REPO_YARN_SETUP=1.3.0
 export REPO_SUBLIME_TEXT_SETUP=9.1.0
 export REPO_DOTFILES=32.5.0
