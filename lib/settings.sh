6a4c1c493d451a20d9d4cd6b1a66ef3dd6c32ea1
Brooke Kuhlmann <brooke@alchemists.io>
2020-05-26 08:37:59 -0600

    Added PixelSnap Homebrew Cask
    Originally, there was no Homebrew Cask support for PixelSnap but now
    there is. This change improves automation and reduces maintenance by
    not having to keep track of new versions of this software.

--- lib/settings.sh
+++ lib/settings.sh
@@ -35,10 +35,6 @@ export HAND_BRAKE_CLI_APP_NAME="HandBrakeCLI"
 export HAND_BRAKE_CLI_VOLUME_NAME="HandBrakeCLI-1.3.0"
 export HAND_BRAKE_CLI_APP_URL="https://download2.handbrake.fr/1.3.0/HandBrakeCLI-1.3.0.dmg"
 
-export PIXEL_SNAP_APP_NAME="PixelSnap 2.app"
-export PIXEL_SNAP_VOLUME_NAME="PixelSnap 2"
-export PIXEL_SNAP_APP_URL="https://updates.getpixelsnap.com/v2/PixelSnap-2-2.3.2.dmg"
-
 export RETROBATCH_HANDLER_APP_NAME="Retrobatch.app"
 export RETROBATCH_HANDLER_APP_URL="https://flyingmeat.com/download/Retrobatch.zip"
 
