32e4a3dee1eef3fc993d772a9c7157e5ac6278a3
Brooke Kuhlmann <brooke@alchemists.io>
2020-11-17 07:47:35 -0700

    Updated to Dotfiles 40.2.0
    [Changes](https://www.alchemists.io/projects/dotfiles/changes.html).

--- lib/settings.sh
+++ lib/settings.sh
@@ -18,7 +18,7 @@ export MRI=2.7.2
 export REPO_RUBY_SETUP=13.1.1
 export REPO_YARN_SETUP=1.6.0
 export REPO_SUBLIME_TEXT_SETUP=14.1.0
-export REPO_DOTFILES=40.0.0
+export REPO_DOTFILES=40.2.0
 
 # Applications
 export COOLANT_APP_NAME="Coolant.app"
