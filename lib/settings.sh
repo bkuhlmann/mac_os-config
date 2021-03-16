8d9ab85c64d04a8ffe781613c5abd67fe9fee522
Brooke Kuhlmann <brooke@alchemists.io>
2021-03-15 21:45:46 -0600

    Updated to Sublime Text Setup 15.1.0
    This version handles the linking of the executable so there is no
    longer a need to maintain that setup within this project.

--- lib/settings.sh
+++ lib/settings.sh
@@ -12,7 +12,7 @@ IFS=$'\n\t'
 export HOMEBREW_CURL_RETRIES=3
 
 # Repositories
-export REPO_SUBLIME_TEXT_SETUP=15.0.0
+export REPO_SUBLIME_TEXT_SETUP=15.1.0
 export REPO_DOTFILES=42.1.0
 
 # Applications
