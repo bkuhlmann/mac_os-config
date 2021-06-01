1f0e122b6fd88c1754275ca534158801c3eea996
Brooke Kuhlmann <brooke@alchemists.io>
2021-06-01 14:26:50 -0600

    Updated to Dotfiles 43.1.0
    [Details](https://www.alchemists.io/projects/dotfiles/changes.html).

--- lib/settings.sh
+++ lib/settings.sh
@@ -13,7 +13,7 @@ export HOMEBREW_CURL_RETRIES=3
 
 # Repositories
 export REPO_SUBLIME_TEXT_SETUP=15.1.0
-export REPO_DOTFILES=43.0.0
+export REPO_DOTFILES=43.1.0
 
 # Applications
 export ELM_APP_NAME="elm"
