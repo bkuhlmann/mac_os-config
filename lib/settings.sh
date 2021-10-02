899692e4f21cad7a27235b90bb5e8d9e72ebba0b
Brooke Kuhlmann <brooke@alchemists.io>
2021-10-02 13:46:43 -0600

    Updated to Sublime Text Setup 18.1.0

--- lib/settings.sh
+++ lib/settings.sh
@@ -12,7 +12,7 @@ IFS=$'\n\t'
 export HOMEBREW_CURL_RETRIES=3
 
 # Repositories
-export REPO_SUBLIME_TEXT_SETUP=18.0.0
+export REPO_SUBLIME_TEXT_SETUP=18.1.0
 export REPO_DOTFILES=44.1.0
 
 # Applications
